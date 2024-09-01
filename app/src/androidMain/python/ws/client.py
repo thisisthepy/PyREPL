899c6bf04dcbadd71b0c1a53bab089835285abb5
BREW <IRACK000@gmail.com>
2024-09-01 12:52:56 +0900

    Feat: Add Websockets

--- app/src/androidMain/python/ws/client.py
+++ app/src/androidMain/python/ws/client.py
@@ -1,25 +1,58 @@
 import websocket
+from model.config import ChatHistory
+import json
 
-def on_message(ws, message):
-    print(f"Received from server: {message}")
+chat_history = ChatHistory()
 
-def on_error(ws, error):
-    print(f"Error: {error}")
+WEBSOCKET_URL = "ws://127.0.0.1:8000/ws"
 
-def on_close(ws, close_status_code, close_msg):
-    print("Connection closed")
 
 def on_open(ws):
-    print("Connection established")
-    ws.send("Hello, Server!")
-    print("Sent to server: Hello, Server!")
+    print("Websocket connection is now open")
+
+    data = get_user_message().strip()
+    if data:
+        ws.send(json.dumps(chat_history))  # chat history 전송
+        ws.send(data)  # user prompt 전송
+        chat_history.append("user", data)  # chat history 업데이트
+        update_screen(data, True)  # 화면 업데이트
+        set_user_message("")  # 입력창 초기화
+
+
+def on_message(ws, message):
+    print("Message received:", message)
+    if message == "<EOS>":
+        ws.close()
+        return
+
+    if message:
+        update_screen(message, False)
+
+
+def on_close(ws, *_):
+    print("Connection closed")
+
+
+def get_user_message():
+    """ should be overridden by the platform-specific implementation """
+    return ""
+
+
+def set_user_message(message: str):
+    """ should be overridden by the platform-specific implementation """
+    pass
+
+
+def update_screen(text: str, user_content: bool = True):
+    """ should be overridden by the platform-specific implementation """
+    pass
 
 
 def run_websocket():
-    ws = websocket.WebSocketApp("ws://localhost:8765",
+    ws = websocket.WebSocketApp(WEBSOCKET_URL,
                                 on_open=on_open,
                                 on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
+                                on_close=on_close,
+                                on_error=lambda w, e: print("Websocket error", e))
 
     ws.run_forever()
